5911546dcb0d23ffdd342c142034b6f6733db32c
DS436 <102336057+DS436@users.noreply.github.com>
2024-04-22 08:34:21 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -6,33 +6,36 @@ function loadChartData() {
     fetch('/get-exercise-data')
         .then(response => response.json())
         .then(data => {
-            var chart = new CanvasJS.Chart("chartContainer", {
-                animationEnabled: true,
-                theme: "light2",
-                title: {
-                    text: "Your Exercise Duration"
-                },
-                axisY: {
-                    title: "Duration in Minutes",
-                    includeZero: false
-                },
-                data: [{
-                    type: "column",
-                    dataPoints: data
-                }]
-            });
-            chart.render();
-
-            updateExerciseList(data);  // Update the list/table of data
+            if (data.length > 0) {
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    animationEnabled: true,
+                    theme: "light2",
+                    title: {
+                        text: "Your Exercise Duration"
+                    },
+                    axisY: {
+                        title: "Duration in Minutes",
+                        includeZero: false
+                    },
+                    data: [{
+                        type: "column",
+                        dataPoints: data
+                    }]
+                });
+                chart.render();
+                updateExerciseList(data);  // Update the list/table of data
+            } else {
+                document.getElementById('chartContainer').innerHTML = 'No data to display';
+            }
         });
 }
 
 function updateExerciseList(data) {
-    const listContainer = document.createElement('ul');
+    const listContainer = document.getElementById('dataList');
+    listContainer.innerHTML = '';  // Clear existing entries
     data.forEach(item => {
         const listItem = document.createElement('li');
         listItem.textContent = `Date: ${item.label}, Duration: ${item.y} minutes`;
         listContainer.appendChild(listItem);
     });
-    document.body.appendChild(listContainer);
 }
